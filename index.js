64dc3865f049b76f421f8e9106e9311b8dceb4b6
Hoang Minh Tam <goddie9x@yahoo.com>
2024-10-15 17:33:35 +0700

    feat: add route for reaction

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 require('dotenv').config();
 
 const express = require('express');
+const getAuthAndPutCurrentUserAuthToBody = require('./utils/middlewares/getAuthAndPutCurrentUserAuthToBody');
+const connectToDiscoveryServer = require('./utils/configs/discovery');
+const router = require('./routes');
 
 const PORT = process.env.PORT||3007;
 const app = express();
 
 app.use(express.json());
+app.use(getAuthAndPutCurrentUserAuthToBody);
+app.use(process.env.APP_PATH||'/api/v1/reactions',router);
+
+connectToDiscoveryServer();
 
 const server = app.listen(PORT,()=>{
     console.log('App running in port '+PORT);
---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const reactionController = require('../controllers/reactionController');
+const mapHealthStatusRoute = require('../utils/eureka/healthStatusRoute');
+
+mapHealthStatusRoute(router);
+router.get('/of-current-user',reactionController.getReactionOfCurrentUser);
+router.get('/of-target',reactionController.getReactionOfTargetWithPagination);
+router.post('/react',reactionController.react);
+router.delete('/cancel/:id',reactionController.cancelReact);
+router.delete('/of-target',reactionController.deleteAllReactOfTarget);
+
+module.exports = router;
